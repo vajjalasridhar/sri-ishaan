8064feb9990662fd8d69b5ce411f7e4b2d1a0733
sridhar yadav <vajjalasridhar15@gmail.com>
2025-06-17 14:35:52 +0530

    updated main.py with list logic

--- ishaan2.py
+++ ishaan2.py
@@ -1,5 +1,10 @@
-n=int(input("enter the number :"))
+"""n=int(input("enter the number :"))
 if n>=18:
     print("he/she is eligible to vote")
 else:
-    print("not !!!!!!!!eligible")
+    print("not !!!!!!!!eligible")"""
+
+list=[1,2,3,4,5,5,6,6]
+print(type(list))
+print(list.append("sridhar"))
+print(list)
